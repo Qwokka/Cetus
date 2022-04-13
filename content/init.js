7623e532b5d6d626b2fc0b459101219b5bb5f676
Jack Baker <jack.baker@exodusintel.com>
2022-04-12 19:58:56 -0700

    Minor fix for pages where Cetus is injected twice

--- content/init.js
+++ content/init.js
@@ -14,7 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-let cetus = null;
+if (typeof cetus === "undefined") {
+    cetus = null;
+}
 
 // Inform the extension when we leave the page
 window.onbeforeunload = function() {
